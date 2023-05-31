7698fc7a640b36793dbbb0066725c71be7d2b6f6
Krishna2128 <133209668+Krishna2128@users.noreply.github.com>
2023-05-31 13:15:03 +0530

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# Get the current date
-current_date=$(date +%Y%m%d)
+# present date
+present_date=$(date +%Y%m%d)
 
 # Destination
 dest="/tmp/backup"
 
-# Create a backup file name with the current date
-backup_file="${current_date}_backup.tar.gz"
+# Crt backup file name with present date
+backup_file="${present_date}_backup.tar.gz"
 
-# Create the backup
+# Creating backup
 tar -czvf "$dest/$backup_file" .
 
-# Print the backup file name
+# Printing backup file name
 echo "Backup created: $backup_file"
 
-# Print backup flie path
+# Printing backup flie path
 echo "backup file path:$dest"
